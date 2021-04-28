5a0f2bfe663f29a347ab889ab786fc0c2d9f983b
raihaninfo <raihanmahmudi35@gmail.com>
2021-04-28 12:25:35 +0600

    add js math function

---
+++ 00_1/math.js
@@ -0,0 +1,15 @@
+console.log(Math.E)
+console.log(Math.PI)
+
+var n = 4.589
+console.log(Math.abs(n))
+console.log(Math.floor(n))
+console.log(Math.ceil(n))
+console.log(Math.round(n))
+
+console.log(Math.max(400, 500))
+console.log(Math.min(400, 500))
+console.log(Math.pow(2,3))
+console.log(Math.sqrt(9))
+
+console.log(Math.round(Math.random()*50 +1))
