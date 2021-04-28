4cf6070bad7442430a0d7f54086e4b37b7e4e876
raihaninfo <raihanmahmudi35@gmail.com>
2021-04-28 11:15:54 +0600

    number datatype added

--- 00_1/datatypes.js
+++ 00_1/datatypes.js
@@ -1,17 +1,22 @@
 /*
-1. primitive type
-2. object type
+    1. primitive type
+    2. object type
 
-primitive type 
-    1. number
-    2. string
-    3. boolean
-    4. undefined
-    5. null
-    
-object type
-    1. Arry
-    2. object
-    3. function 
+    primitive type 
+        1. number
+        2. string
+        3. boolean
+        4. undefined
+        5. null
+        
+    object type
+        1. Arry
+        2. object
+        3. function 
 
 */
+
+var num1 = 23
+var num2 = 3.4
+
+console.log(num1+num2)
