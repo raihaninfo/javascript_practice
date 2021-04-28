c2a73c641a43436719f773a95b2b54477999b6df
raihaninfo <raihanmahmudi35@gmail.com>
2021-04-28 11:54:02 +0600

    oporetors added

---
+++ 00_1/oporetors.js
@@ -0,0 +1,13 @@
+// arithmetic operator
+//  +, -, *, / % ++ --
+
+var a = 10
+var b = 3
+console.log(a+b)
+console.log(a-b)
+console.log(a/b)
+console.log(a%b)
+
+// incremental 
+
+console.log(a++)
