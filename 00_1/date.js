5caac4af0818c91b0434afdc6bec577aad3402f9
raihaninfo <raihanmahmudi35@gmail.com>
2021-04-28 12:32:17 +0600

    js date function added

---
+++ 00_1/date.js
@@ -0,0 +1,8 @@
+var date = new Date()
+console.log(date.toString())
+console.log(date.toTimeString())
+console.log(date.toLocaleString())
+console.log(date.getFullYear())
+console.log(date.getMonth())
+console.log(date.getDate())
+console.log(date.getMinutes())
