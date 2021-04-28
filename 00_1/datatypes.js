8c6ecd0d8577c6f5d6c640d5f6939b030e7038b7
raihaninfo <raihanmahmudi35@gmail.com>
2021-04-28 11:11:19 +0600

    create file

---
+++ 00_1/datatypes.js
@@ -0,0 +1,17 @@
+/*
+1. primitive type
+2. object type
+
+primitive type 
+    1. number
+    2. string
+    3. boolean
+    4. undefined
+    5. null
+    
+object type
+    1. Arry
+    2. object
+    3. function 
+
+*/
