73a913d4aa8bd5ea0291860a670bafa94712ca16
raihaninfo <raihanmahmudi35@gmail.com>
2021-04-30 17:59:35 +0600

    forloop exampull added

--- 00_3/forLoop.js
+++ 00_3/forLoop.js
@@ -1,5 +1,15 @@
 // for loop
 var i = 0;
-for( i; i <100; i ++){
-    console.log((i+1) + " Raihan")
+for( i; i <=100; i++){
+    if(i %2 ==1){
+    console.log(i)
+    }
+    
 }
+
+var sum = 0;
+for (var i = 1; i<10; i ++) {
+    console.log(sum + " + " + i + " = " + (sum+i))
+   sum +=i
+}
+console.log('result = ' + sum)
