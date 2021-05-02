f8fde6103409b47bd148de128e22d7544e9d3090
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-02 19:40:40 +0600

    add while loop

---
+++ 00_3/whileLoop.js
@@ -0,0 +1,18 @@
+
+// var i = 0
+// while(i < 10){
+//     console.log("Raihan")
+//     i++
+// }
+
+var isRunning = true
+
+while (isRunning) {
+    var rand = Math.floor(Math.random()*10 +1)
+    if (rand == 9){
+        console.log("You have Win")
+        isRunning = false
+    }else{
+        console.log("You have got " + rand)
+    }
+}
