a288f93a21731ecbef1a8439208c0725a79f6810
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-02 19:59:41 +0600

    nestedloop

---
+++ 00_3/nestedLoop.js
@@ -0,0 +1,17 @@
+// var n = 10
+// for (var i = 1; i <= n; i ++){
+//     var result= ""
+//    for(var j = 1; j<=i; j++){
+//        result += j + " "
+//    }
+//    console.log(result)
+// }
+
+var n = 10
+for (i= 1; i <= n; i++){
+    var result = ""
+    for (var j =1; j<=n; j++ ){
+        result += "* "
+    }
+    console.log(result)
+}
