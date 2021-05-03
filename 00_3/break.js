8a4b2ca3deaeb6d00ade295122b1aa13131c5587
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-03 22:01:08 +0600

    break stetment added

--- 00_3/break.js
+++ 00_3/break.js
@@ -1,10 +1,10 @@
-var isRunning = true
+// var isRunning = true
 
-while (isRunning) {
+while (true) {
     var rand = Math.floor(Math.random()*10 +1)
     if (rand == 9){
         console.log("You have Win")
-        isRunning = false
+        break
     }else{
         console.log("You have got " + rand)
     }
