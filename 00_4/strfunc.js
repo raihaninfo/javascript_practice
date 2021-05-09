f3c54b55d731a3cf8c20fa750b94221d12ed7b2b
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-10 00:02:47 +0600

    learn string function

---
+++ 00_4/strfunc.js
@@ -0,0 +1,23 @@
+// String Function
+
+var a = "i am";
+var b = " Raihan";
+var c = a.concat(b);
+var d = c.substr(5)
+console.log(c.charAt(5))
+console.log(c.startsWith("i am"))
+console.log(d)
+
+// Case Change
+
+var myName = "raihan";
+console.log(myName.toUpperCase())
+
+// space remove in string 
+
+var n = "   ljsdf ls     ";
+console.log(n.trim())
+
+// string to array
+var r = "my name is raihan";
+console.log(r.split(" "))
