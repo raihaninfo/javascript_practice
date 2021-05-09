893625e58879efd2a7ac8ebe05a059ee930bc0f2
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-09 23:48:53 +0600

    string comparison larning

--- 00_4/string.js
+++ 00_4/string.js
@@ -12,3 +12,13 @@ var str3 = "This is a \"String\" ";
 var str4 = "This is a \nString ";
 var str5 = "This is a \tString ";
 console.log(str3, str4, str5)
+
+
+// string comparison
+// z, y, x fast
+// Z, Y, X second
+
+var a = "abc"
+var b = "bca"
+
+console.log(a === b)
