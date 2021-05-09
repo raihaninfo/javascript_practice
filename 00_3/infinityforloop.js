a84e2d1aaa9f2da00ba8e9c78d5b64af12e56c63
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-09 23:25:01 +0600

    infinity loop

---
+++ 00_3/infinityforloop.js
@@ -0,0 +1,10 @@
+
+for (; ;){
+    var rand = Math.floor(Math.random()*10 +1)
+    if (rand == 9){
+        console.log("You have Win")
+        break
+    }else{
+        console.log("You have got " + rand)
+    }
+}
