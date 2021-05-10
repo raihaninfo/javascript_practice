ec643cc538f9e72ce086a652ca41b4a40371af5c
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-10 22:54:40 +0600

    code formating

--- 00_5/search.js
+++ 00_5/search.js
@@ -1,5 +1,4 @@
 // Search Data in array
-
 var arr = [2, 3, 4, 5, 56, 7, 8, 44, 33, 22];
 
 var fine = 10;
@@ -11,6 +10,7 @@ for (var i = 0 ; i < arr.length; i ++){
         isFound = true;
     }
 }
+
 if (!isFound){
     console.log("Data Not Found")
 }
