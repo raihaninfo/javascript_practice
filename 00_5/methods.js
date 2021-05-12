3d0d57c662a27d8efefa35a44d335d151fe1b162
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-12 23:45:17 +0600

    array method function

---
+++ 00_5/methods.js
@@ -0,0 +1,14 @@
+var arr = [2, 3, 4, 5, 6]
+var arr2 = [32, 34, 65]
+
+// join
+console.log(arr.join(", "));
+
+//fill
+console.log(arr.fill(0));
+
+// concat method
+console.log(arr.concat(arr2));
+
+//array chack
+console.log(Array.isArray(arr));
