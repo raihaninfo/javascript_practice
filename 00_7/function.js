3b5aee1f8cac29c49373d39c7d3a9fc124e7ce9e
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-18 15:14:39 +0600

    function in javascript

---
+++ 00_7/function.js
@@ -0,0 +1,13 @@
+// chapter sever function in javascript
+
+// var date = new Date()
+// var year = date.getFullYear()
+// console.log(year)\
+
+function funcName(){
+    var a = 10;
+    var b = 20;
+    console.log(a+b)
+}
+
+funcName()
