8a7d01501f08123f9a45e51b981311867adf649c
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-19 22:01:15 +0600

    sum of array function

--- 00_7/argument.js
+++ 00_7/argument.js
@@ -4,3 +4,22 @@ function add(a, b){
 }
 add(2,3)
 add(10, 20)
+
+var arr1 = [2, 3, 4]
+var arr2 = [3, 5, 7]
+// var sum1 = 0;
+// for(var i = 0; i < arr1.length; i ++){
+//     sum1 += arr1[i]
+// }
+// console.log(sum1)
+
+function sumOfArray(arr){
+    var sum = 0;
+    for(var i = 0; i < arr.length; i ++){
+        sum += arr[i]
+    }
+    console.log(sum)
+}
+
+sumOfArray(arr1)
+sumOfArray(arr2)
