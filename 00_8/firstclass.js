01f207870eadb51921fa9c87d6a9a08d070af1c2
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-22 17:29:23 +0600

    comment added

--- 00_8/firstclass.js
+++ 00_8/firstclass.js
@@ -1,8 +1,9 @@
 // First class function 
-
+// 
 // 1. A function can be stored in a variable.
 // 2. A function can be stored in an Array
 // 3. A function can be stored in an Object
 // 4. We can create function as Need
 // 5. we can pass function as an Arguments
 // 6. we can return functions from another function 
+
