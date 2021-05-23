31175979ca472f672795b70362f36f6159e4ae37
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-23 12:03:37 +0600

    function call back

---
+++ 00_8/callback.js
@@ -0,0 +1,15 @@
+// 
+
+function sample(a, b){
+    var c = a +b
+    var d = a -b
+    var result = sum(c, d)
+    return result
+
+}
+
+function sum(a, b){
+    return a+b
+}
+
+console.log(sample(6,5))
