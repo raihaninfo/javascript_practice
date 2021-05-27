2ebc7cd4201b355c532db320ebe8643587817d3d
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-27 22:02:07 +0600

    nested scope

--- 00_9/scope.js
+++ 00_9/scope.js
@@ -3,6 +3,14 @@ var x = 55
 function test() {
     var x = 45
     console.log(x)
+
+    function nested(){
+        var y = 65
+        console.log(x)
+    }
+
+    // console.log(y)
+    nested()
 }
 
 test()
