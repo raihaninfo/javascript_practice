83fa889be156d0de693fdf06a47c9e38f6bdc93e
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-29 21:35:06 +0600

    play with dom

--- 11/index.js
+++ 11/index.js
@@ -11,11 +11,11 @@
 //     }
 // }
 
-var createRect = function(width, height){
+var createRect = function (width, height) {
     return {
         width: width,
         height: height,
-    
+
         draw: function () {
             console.log("i am a rectangle")
             this.printProperties()
@@ -33,19 +33,41 @@ var rect2 = createRect(23, 45)
 rect2.draw()
 
 
-var Recttangle = function(width, height){
-    this.width= width
-    this.height= height
+var Recttangle = function (width, height) {
+    this.width = width
+    this.height = height
 
-    this.draw = function(){
+    this.draw = function () {
         console.log("I am a reactangle")
         this.printProperties()
         console.log(this)
     }
-    this.printProperties = function() {
+    this.printProperties = function () {
         console.log("My width is " + this.width + " And Height " + this.height)
     }
 }
 
-var rect3 = new Recttangle(23,45)
+var rect3 = new Recttangle(23, 45)
 rect3.draw()
+
+var n = 10
+
+function change(n) {
+    n = n + 100
+    console.log(n)
+}
+change(n)
+console.log(n)
+
+var obj = {
+    a: 10,
+    b: 20
+}
+
+function changeMe(obj) {
+    obj.a = obj.a + 100
+    obj.b = obj.b + 100
+    console.log(obj)
+}
+
+changeMe(obj)
