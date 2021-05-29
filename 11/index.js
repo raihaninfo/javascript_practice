d44d946b8f2fe2e5114dec452b956fdd7031bafb
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-29 11:37:55 +0600

    oop constructor function

--- 11/index.js
+++ 11/index.js
@@ -31,3 +31,21 @@ rect1.draw()
 
 var rect2 = createRect(23, 45)
 rect2.draw()
+
+
+var Recttangle = function(width, height){
+    this.width= width
+    this.height= height
+
+    this.draw = function(){
+        console.log("I am a reactangle")
+        this.printProperties()
+        console.log(this)
+    }
+    this.printProperties = function() {
+        console.log("My width is " + this.width + " And Height " + this.height)
+    }
+}
+
+var rect3 = new Recttangle(23,45)
+rect3.draw()
