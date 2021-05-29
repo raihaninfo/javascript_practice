70e22e8db34ac2b0d7507d04281d9d3c7a4fa94e
raihaninfo <raihanmahmudi35@gmail.com>
2021-05-29 11:27:00 +0600

    fectory pettan

--- 11/index.js
+++ 11/index.js
@@ -1,24 +1,33 @@
-var rect = {
-    width: 100,
-    height: 50,
+// var rect = {
+//     width: 100,
+//     height: 50,
 
-    draw: function () {
-        console.log("i am a rectangle")
-        this.printProperties()
-    },
-    printProperties: function () {
-        console.log("My width is " + this.width + " And Height " + this.height)
+//     draw: function () {
+//         console.log("i am a rectangle")
+//         this.printProperties()
+//     },
+//     printProperties: function () {
+//         console.log("My width is " + this.width + " And Height " + this.height)
+//     }
+// }
+
+var createRect = function(width, height){
+    return {
+        width: width,
+        height: height,
+    
+        draw: function () {
+            console.log("i am a rectangle")
+            this.printProperties()
+        },
+        printProperties: function () {
+            console.log("My width is " + this.width + " And Height " + this.height)
+        }
     }
 }
 
-rect.draw()
-rect.height = 100;
-rect.draw()
+var rect1 = createRect(10, 4)
+rect1.draw()
 
-var another = {
-    width: 45,
-    height: 55,
-    print: rect.printProperties
-}
-
-another.print()
+var rect2 = createRect(23, 45)
+rect2.draw()
