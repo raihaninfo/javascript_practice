5e44691d96c8e67adaa8cca9300e7ad7ae39bab5
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-02 22:56:45 +0600

    es6 set data stucture

--- webpack-config/src/scripts/index.js
+++ webpack-config/src/scripts/index.js
@@ -1,28 +1,6 @@
-import '../styles/index.css'
-import {
-    ABC
-} from './test'
+// let set = new Set([1, 2, 3])
+let set = new Set()
+set.add(5)
+set.add(6)
 
-const fn = () => {
-    console.log("i am Fn")
-}
-
-fn()
-
-function sum(a, b) {
-    return a + b
-}
-
-console.log(sum(2, 3));
-
-console.log(ABC)
-
-class x {
-    a = 10
-    xyz() {
-        console.log(this.a)
-    }
-}
-
-let a = new x()
-a.xyz()
+console.log(set)
