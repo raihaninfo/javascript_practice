63064afd7cda758d41ef3ee69119243f4e853ad2
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-03 11:40:49 +0600

    class method

--- webpack-config/src/scripts/index.js
+++ webpack-config/src/scripts/index.js
@@ -1,12 +1,19 @@
+
 class Rectangle {
     constructor(width, height) {
         this.width = width
         this.height = height
     }
+    name = "raihan"
     draw() {
         console.log('Drawing...')
+    }
+
+    test() {
+        console.log(323)
     }
 }
 
 let rect1 = new Rectangle(23, 45)
 console.log(rect1)
+console.log(rect1.test())
