b98d124e8ce68abb813fe5eb3d362a7a90c9f61a
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-08 15:20:23 +0600

    dom mousemove event

--- DOM/main.js
+++ DOM/main.js
@@ -3,3 +3,20 @@ let title = document.getElementById('title')
 title.style.color = '#f00'
 title.style.fontSize = '2.4rem'
 title.style.background = "black"
+
+let btn = document.getElementById('btn')
+
+// btn.onclick = function(e){
+//     console.log(e)
+// }
+
+btn.addEventListener('click', function (e) {
+    console.log(e)
+})
+
+let box = document.getElementById('box')
+box.addEventListener('mousemove', function(e){
+    document.getElementById('x-value').innerHTML = e.clientX
+    document.getElementById('y-value').innerHTML = e.clientY
+})
+// console.log(btn)
