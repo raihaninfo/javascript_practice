82efb698bc6fe40d928b3a143e83a9301a5a2caf
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-08 22:14:20 +0600

    get value in  chackBox and input box

--- DOM/main.js
+++ DOM/main.js
@@ -5,3 +5,28 @@ Name.addEventListener('keypress', function (event) {
         event.target.value = ''
     }
 })
+
+let skills = document.getElementsByName('skills');
+let result = document.getElementById('result');
+
+let checkedSkills = [];
+[...skills].forEach(skill => {
+    skill.addEventListener('change', function (event) {
+        if (event.target.checked) {
+            checkedSkills.push(event.target.value)
+            outputSkills(result, checkedSkills)
+        } else {
+            let ind = checkedSkills.indexOf(event.target.value)
+            checkedSkills.splice(ind, 1)
+            outputSkills(result, checkedSkills)
+        }
+    })
+})
+
+function outputSkills(parent, skills) {
+    let result = ''
+    skills.forEach((skill, index) => {
+        result += `(${index + 1 })   ${skill}   `
+    })
+    parent.innerHTML = result
+}
