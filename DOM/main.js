9847be30fa62988ec3baee15e1d3db9836a8c62d
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-08 20:57:02 +0600

    remove list in click

--- DOM/main.js
+++ DOM/main.js
@@ -11,15 +11,31 @@ let btn = document.getElementById('btn')
 // }
 let list = document.getElementById('list')
 
+
 btn.addEventListener('click', function (e) {
     let item = list.lastElementChild.cloneNode(true)
     item.innerHTML = 'Newly Created Item'
     list.appendChild(item)
 })
 
+
 let box = document.getElementById('box')
-box.addEventListener('mousemove', function(e){
+box.addEventListener('mousemove', function (e) {
     document.getElementById('x-value').innerHTML = e.clientX
     document.getElementById('y-value').innerHTML = e.clientY
+    if (e.clientY == 400 || e.clientX == 400) {
+        alert('Your position 400')
+    }
 })
+
+
+// remove list in click
+list.addEventListener('click', function (e) {
+    if (this.contains(e.target)) {
+        e.target.remove()
+    }
+})
+
+
+
 // console.log(btn)
