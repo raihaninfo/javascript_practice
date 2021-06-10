8f40801defd681b830c8f4ad3880e489174620b2
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-10 21:34:17 +0600

    calculator functin complete

---
+++ Calculator/index.js
@@ -0,0 +1,24 @@
+let screen = document.getElementById('screen')
+button = document.querySelectorAll('button')
+let screenText = ''
+for (items of button) {
+    items.addEventListener('click', function (e) {
+        let buttonText = e.target.innerHTML
+        if (buttonText == 'X') {
+            screenText = '*';
+            screenText += buttonText = ''
+            screen.value = screenText
+        } else if (buttonText == 'C') {
+            screenText = '';
+            screenText += buttonText = ''
+            screen.value = screenText
+        } else if (buttonText == '=') {
+            screen.value = eval(screenText)
+        } else {
+            screenText += buttonText
+            screen.value = screenText
+        }
+
+
+    })
+}
