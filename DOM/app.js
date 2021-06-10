7ed89c791709385a7b97bb3097ffb4f6dfdfd752
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-10 23:37:49 +0600

    form valided complete

--- DOM/app.js
+++ DOM/app.js
@@ -2,11 +2,29 @@ let form = document.getElementById('form')
 
 form.addEventListener('submit', function (e) {
     e.preventDefault()
+    let isValid = false
     let formData = {};
     [...this.elements].forEach(el => {
         if (el.type !== 'submit') {
-            formData[el.name] = el.value
+            isValid = validetor(el)
+            if (isValid) {
+                formData[el.name] = el.value
+            } else {
+                // alert(`${el.name} Is Required`)
+                el.classList.add('is-invalid')
+            }
         }
     })
-    console.log(formData)
+    if (isValid) {
+        console.log(formData)
+        this.reset()
+    }
 })
+
+function validetor(ele) {
+    if (!ele.value) {
+        return false
+    }
+
+    return true
+}
