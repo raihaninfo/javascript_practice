a97d484eea0ba42e450cd1c3e423d2dab91d654b
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-12 15:03:59 +0600

    show post in fetch api

--- DOM/api/app.js
+++ DOM/api/app.js
@@ -1,1 +1,25 @@
-console.log('fetch api')
+let load = document.getElementById('load')
+let postlist = document.getElementById('post')
+
+const URL = 'https://jsonplaceholder.typicode.com/posts'
+
+load.addEventListener('click', function () {
+    fetch(URL)
+        .then(response => response.json())
+        .then(post => {
+
+            post.forEach(post => {
+                let listItem = listItemGenatetor(post)
+                postlist.appendChild(listItem)
+            })
+        })
+        .catch(e => console.log(e.message))
+})
+
+function listItemGenatetor(item) {
+    let li = document.createElement('li')
+    li.className = 'list-group-item'
+    li.innerHTML = item.title
+
+    return li
+}
