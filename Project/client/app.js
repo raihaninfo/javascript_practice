b8f31af6b1a61cbcde71b167729af24699a2799e
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-14 15:32:22 +0600

    weather set in dom

--- Project/client/app.js
+++ Project/client/app.js
@@ -14,7 +14,7 @@ const masterHistory = document.getElementById('master-history')
 const API_KEY = '98cc9e9bd0aed433f384ad9b167ad962'
 const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`
 const ICON_URL = 'https://openweathermap.org/img/w/'
-const DEFAULT_CITY = 'kushtia,bd'
+const DEFAULT_CITY = 'Dhaka,bd'
 
 window.onload = function () {
     navigator.geolocation.getCurrentPosition(s => {
@@ -29,5 +29,35 @@ function getWeatherData(city = DEFAULT_CITY, coords) {
     city == null ?
         url = `${url}&lat=${coords.latitude}&lon=${coords.longitude}` :
         url = `${url}&q=${city}`
-    console.log(url)
+    axios.get(url)
+        .then(({
+            data
+        }) => {
+            let weather = {
+                icon: data.weather[0].icon,
+                name: data.name,
+                country: data.sys.country,
+                main: data.weather[0].main,
+                description: data.weather[0].description,
+                temp: data.main.temp,
+                pressure: data.main.pressure,
+                humidity: data.main.humidity
+            }
+            setWeather(weather)
+        })
+        .catch(e => {
+            console.log(e)
+            alert('City Not Found')
+        })
+}
+
+function setWeather(weather) {
+    condition.src = `${ICON_URL}${weather.icon}.png`
+    city.innerHTML = weather.name
+    country.innerHTML = weather.country
+    mainText.innerHTML = weather.main
+    description.innerHTML = weather.description
+    temp.innerHTML = weather.temp
+    pressure.innerHTML = weather.pressure
+    humidity.innerHTML = weather.humidity
 }
