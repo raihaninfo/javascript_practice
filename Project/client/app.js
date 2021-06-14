befb0680eaa4d064edf0a82cf73c76a7ad5352aa
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-14 15:05:22 +0600

    current location get in browser

--- Project/client/app.js
+++ Project/client/app.js
@@ -1,1 +1,33 @@
-let api = '98cc9e9bd0aed433f384ad9b167ad962';
+const condition = document.getElementById('condition')
+const city = document.getElementById('city')
+const country = document.getElementById('country')
+const mainText = document.getElementById('main')
+const description = document.getElementById('description')
+const temp = document.getElementById('temp')
+const pressure = document.getElementById('pressure')
+const humidity = document.getElementById('humidity')
+
+const cityInput = document.getElementById('city-input')
+const historyElm = document.getElementById('history')
+const masterHistory = document.getElementById('master-history')
+
+const API_KEY = '98cc9e9bd0aed433f384ad9b167ad962'
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`
+const ICON_URL = 'https://openweathermap.org/img/w/'
+const DEFAULT_CITY = 'kushtia,bd'
+
+window.onload = function () {
+    navigator.geolocation.getCurrentPosition(s => {
+        getWeatherData(null, s.coords)
+    }, e => {
+        getWeatherData()
+    })
+}
+
+function getWeatherData(city = DEFAULT_CITY, coords) {
+    let url = BASE_URL
+    city == null ?
+        url = `${url}&lat=${coords.latitude}&lon=${coords.longitude}` :
+        url = `${url}&q=${city}`
+    console.log(url)
+}
