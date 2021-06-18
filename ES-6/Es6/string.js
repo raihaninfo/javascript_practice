902d6b3bf8ea30afd8d2dcdb6ea53bab60c40099
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-18 20:06:31 +0600

    string templete

---
+++ ES-6/Es6/string.js
@@ -0,0 +1,12 @@
+// backtik
+// let str = `ljsdlf lsdjfl sdlf 
+// lskdfjls dfl
+//         ljsdf las fd
+//     lsdf lf `
+
+console.log(str)
+
+let age = 22
+let name = 'Md Abu Raihan'
+
+console.log(`My name is ${name} and my age is ${age}`)
