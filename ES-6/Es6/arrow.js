e5c3dc7a69cc41f9ad0431bf8323e5135bba9392
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-18 20:54:46 +0600

    arrow functin

---
+++ ES-6/Es6/arrow.js
@@ -0,0 +1,17 @@
+let add = (a, b)=>{
+    return a + b
+}
+
+console.log(add(2,3))
+
+// let add2 = ()=>{
+
+// }
+
+let add2 = (a,b)=> a+b;
+
+console.log(add2(3,5))
+
+let sqr = x => x*x
+
+console.log(sqr(3))
