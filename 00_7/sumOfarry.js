ba1234a195bd145e90dc1e493a6464c9f0a5d307
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-18 11:41:57 +0600

    Style: code format

--- 00_7/sumOfarry.js
+++ 00_7/sumOfarry.js
@@ -1,6 +1,6 @@
-var a = [2,3,4,5]
-var b = [8,5,6,7]
-var c = [23,45,33]
+var a = [2, 3, 4, 5]
+var b = [8, 5, 6, 7]
+var c = [23, 45, 33]
 
 // var sum = 0
 // for(var i = 0; i<a.length; i++){
@@ -14,10 +14,10 @@ sumOfArray(a)
 sumOfArray(b)
 sumOfArray(c)
 
-function sumOfArray(arr){
+function sumOfArray(arr) {
     sum = 0
-    for(var i = 0; i<arr.length; i ++){
-        sum+=arr[i]
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i]
     }
     console.log(sum)
 }
