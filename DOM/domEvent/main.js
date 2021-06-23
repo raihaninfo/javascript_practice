35318f4949ad45c2b92fbc967e40c8b0e5b8428a
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-24 00:53:31 +0600

    dom aplying js

---
+++ DOM/domEvent/main.js
@@ -0,0 +1,7 @@
+let body = document.querySelector('h2');
+let span = document.getElementById('span')
+
+body.addEventListener('click', function (e) {
+    console.log('Click hoyse re...')
+    span.innerHTML = 'ey tumi click korle kano ?'
+})
