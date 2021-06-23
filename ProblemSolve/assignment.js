997a79cac9f74863a4b31e6341631bb0e41b1580
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-23 16:05:52 +0600

    problem 2, wood calclator complete

--- ProblemSolve/assignment.js
+++ ProblemSolve/assignment.js
@@ -17,7 +17,18 @@ console.log(feetToMile(58558))
 
 
 // wood calculator
+/**
+ * আমরা ধরে নিচ্ছি একটি চেয়ারে ১ ছেপটি কাট লাগে, এবং একটি টেবিলে ২ ছেপটি এবং একটু খাটে ৫ ছেপটি, সেই হিসাবে আমাদের function কাজ করবে। 
+ */
 
+function woodCalculator(chear, table, khat) {
+    let chearCl = chear * 1;
+    let tableCl = table * 2;
+    let khatCl = khat * 5;
+    return chearCl + tableCl + khatCl
+}
+
+console.log(woodCalculator(1, 3, 4))
 
 // brik calculator
 
