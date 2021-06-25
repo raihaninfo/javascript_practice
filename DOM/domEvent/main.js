e8b8a9f2ecaddfdc35d5f7b814409cf9d61b7d18
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-25 22:18:54 +0600

    add btn

--- DOM/domEvent/main.js
+++ DOM/domEvent/main.js
@@ -1,7 +1,10 @@
-let body = document.querySelector('h2');
+let header = document.querySelector('h2');
 let span = document.getElementById('span')
 
-body.addEventListener('click', function (e) {
+header.addEventListener('click', function (e) {
     console.log('Click hoyse re...')
     span.innerHTML = 'ey tumi click korle kano ?'
 })
+
+let body = document.getElementsByTagName('body')
+
