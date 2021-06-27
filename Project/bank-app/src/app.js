00c23e7d3e9ffe35711fa2c37908000823999f58
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-27 16:26:29 +0600

    deposit number and total number sum

--- Project/bank-app/src/app.js
+++ Project/bank-app/src/app.js
@@ -9,3 +9,28 @@ login.addEventListener('click', function () {
     loginArea.style.display = 'none';
     transactionArea.style.display = 'block'
 })
+
+
+
+let depositBtn = document.getElementById('deposit-btn');
+
+depositBtn.addEventListener('click', function () {
+    let depositValue = document.getElementById('deposit-value').value;
+    if (depositValue == 0 || null) {
+        depositValue = '0';
+    }
+    let depositFil = parseFloat(depositValue)
+
+    let = depositAmount = document.getElementById('deposit-amount').innerText
+    let = depositAmountBox = parseFloat(depositAmount)
+    let totalDeposit = depositFil + depositAmountBox;
+
+    document.getElementById('deposit-amount').innerText = totalDeposit;
+    document.getElementById('deposit-value').value = ''
+
+    let totalBalance = document.getElementById('total-balance').innerText;
+    let totalBalanceNum = parseFloat(totalBalance)
+    let totalBalanceSum = totalBalanceNum + depositFil;
+
+    document.getElementById('total-balance').innerText = totalBalanceSum;
+})
