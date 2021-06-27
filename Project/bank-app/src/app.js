05182ddece7b319da4e187133516860f904d4804
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-28 00:50:37 +0600

    withdraw function and total amount successfully work

--- Project/bank-app/src/app.js
+++ Project/bank-app/src/app.js
@@ -13,7 +13,6 @@ login.addEventListener('click', function () {
 
 
 let depositBtn = document.getElementById('deposit-btn');
-
 depositBtn.addEventListener('click', function () {
     let depositValue = document.getElementById('deposit-value').value;
     if (depositValue == 0 || null) {
@@ -24,6 +23,29 @@ depositBtn.addEventListener('click', function () {
     document.getElementById('deposit-value').value = ''
     updateSpam('total-balance', depositFil)
 })
+
+let withdrawBtn = document.getElementById('withdraw-btn')
+withdrawBtn.addEventListener('click', function () {
+    let withdrawAmount = document.getElementById('withdraw-fil').value;
+    if (withdrawAmount == 0 || null) {
+        withdrawAmount = '0';
+    }
+    let withdrawNumber = parseFloat(withdrawAmount)
+    //withdrawUpdate('withdraw-amount', withdrawNumber)
+    updateSpam('withdraw-amount', withdrawNumber)
+    document.getElementById('withdraw-fil').value = ''
+    withdrawUpdate('total-balance', withdrawNumber)
+
+})
+
+function withdrawUpdate(id, withdrawNumber) {
+    let withdraw = document.getElementById(id).innerText
+    let withdrawBalance = parseFloat(withdraw)
+    let totalWithdraw = withdrawBalance - withdrawNumber
+    document.getElementById(id).innerText = totalWithdraw;
+}
+
+
 
 function updateSpam(id, depositFil) {
     let totalBalance = document.getElementById(id).innerText;
