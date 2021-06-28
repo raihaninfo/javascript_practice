b45ca1d9592dbfd818f6d185c855f70b6585a1f1
raihaninfo <raihanmahmudi35@gmail.com>
2021-06-29 01:23:00 +0600

    number pad complete

--- Project/pin-matcher/app.js
+++ Project/pin-matcher/app.js
@@ -5,3 +5,23 @@ generatePin.addEventListener('click', () => {
     let random = Math.round(1000 + Math.random() * 9000)
     pin.value = random
 })
+
+
+let screen = document.getElementById('pin-screen')
+button = document.querySelectorAll('.button')
+let screenText = '';
+for (items of button) {
+    items.addEventListener('click', function (e) {
+        let buttonText = e.target.innerHTML
+        if (buttonText == 'C') {
+            screenText = '';
+            screenText += buttonText = ''
+            screen.value = screenText
+        } else if (buttonText == '&lt;') {
+            
+        } else {
+            screenText += buttonText
+            screen.value = screenText
+        }
+    })
+}
