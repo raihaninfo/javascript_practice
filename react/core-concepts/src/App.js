04eeeb51be48aa6ad3285b3b9dadec7d0910614f
raihaninfo <raihanmahmudi35@gmail.com>
2021-07-06 00:17:48 +0600

    body text change

--- react/core-concepts/src/App.js
+++ react/core-concepts/src/App.js
@@ -1,9 +1,27 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const myName = {
+    name : "Raihan",
+    job: "Web Developer"
+  }
   return (
-    <div>
-      <h2 className="text-center">This is React App</h2>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h2>My Name is {myName.name} and i'm {myName.job}</h2>
+        <a
+          className="App-link"
+          href="/"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
