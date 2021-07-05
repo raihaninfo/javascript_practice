f59e34e6294d9e15d4f2f3cfa75f5f98972322c9
raihaninfo <raihanmahmudi35@gmail.com>
2021-07-06 00:40:37 +0600

    file rename, js to jsx

---
+++ react/core-concepts/src/App.jsx
@@ -0,0 +1,29 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const myName = {
+    name: "Raihan",
+    job: "Web Developer",
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident suscipit cumque sapiente totam delectus soluta vitae dolorum, perferendis id rerum exercitationem? Dignissimos dolor recusandae pariatur ab esse similique veniam eum?</p>
+      <div><p>ll</p></div>
+        <h2>
+          My Name is {myName.name} and i'm {myName.job}
+        </h2>
+        <a className="App-link" href="/" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
