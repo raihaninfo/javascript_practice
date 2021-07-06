df5f3989554de9f1715136ac4e278550f2b8845d
raihaninfo <raihanmahmudi35@gmail.com>
2021-07-06 15:34:35 +0600

    dinamic data

--- react/core-concepts/src/App.jsx
+++ react/core-concepts/src/App.jsx
@@ -8,23 +8,26 @@ function App() {
   };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident suscipit cumque sapiente totam delectus soluta vitae dolorum, perferendis id rerum exercitationem? Dignissimos dolor recusandae pariatur ab esse similique veniam eum?</p>
-      <div><p>ll</p></div>
-        <h2>
-          My Name is {myName.name} and i'm {myName.job}
-        </h2>
-        <a className="App-link" href="/" rel="noopener noreferrer">
-          Learn React,
-        </a>
-        <p>i'm learning react js</p>
-      </header>
+      <Parson dis="Kushtia" name="Rubel"></Parson>
+      <Parson dis="Pabna" name="Hasan"></Parson>
+      <Parson dis="khulna" name="Ruhul Amin"></Parson>
     </div>
   );
+}
+
+function Parson(props){
+  const personStyle={
+    border:'2px solid red',
+    margin:'10px'
+  };
+
+  return (<div style={personStyle}>
+    <h2>My Name Is {props.name} </h2>
+    <h3>I Im Student</h3>
+    <p>from : {props.dis} </p>
+  </div>
+
+    )
 }
 
 export default App;
