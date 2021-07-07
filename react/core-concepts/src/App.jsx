8966cef4ac5f62cc32f204e7275e6428c61c9575
raihaninfo <raihanmahmudi35@gmail.com>
2021-07-07 23:32:07 +0600

    all data get in api

--- react/core-concepts/src/App.jsx
+++ react/core-concepts/src/App.jsx
@@ -1,65 +1,20 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const Persion = [
-    {
-      "name": "Leanne Graham",
-      "username": "Bret",
-      "email": "Sincere@april.biz"
-    },
-    {
-      "name": "Ervin Howell",
-      "username": "Antonette",
-      "email": "Shanna@melissa.tv"
-    },
-    {
-      "name": "Clementine Bauch",
-      "username": "Samantha",
-      "email": "Nathan@yesenia.net"
-    },
-    {
-      "name": "Patricia Lebsack",
-      "username": "Karianne",
-      "email": "Julianne.OConner@kory.org"
-    },
-    {
-      "name": "Chelsey Dietrich",
-      "username": "Kamren",
-      "email": "Lucio_Hettinger@annie.ca"
-    },
-    {
-      "name": "Mrs. Dennis Schulist",
-      "username": "Leopoldo_Corkery",
-      "email": "Karley_Dach@jasper.info"
-    },
-    {
-      "name": "Kurtis Weissnat",
-      "username": "Elwyn.Skiles",
-      "email": "Telly.Hoeger@billy.biz"
-    },
-    {
-      "name": "Nicholas Runolfsdottir V",
-      "username": "Maxime_Nienow",
-      "email": "Sherwood@rosamond.me",
-    },
-    {
-      "name": "Glenna Reichert",
-      "username": "Delphine",
-      "email": "Chaim_McDermott@dana.io"
-    },
-    {
-      "name": "Clementina DuBuque",
-      "username": "Moriah.Stanton",
-      "email": "Rey.Padberg@karina.biz"
-    }
-  ]
+
+  const [user, setUser] = useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data =>setUser(data))
+  },[])
 
   return (
     <div className="App">
      {
-     Persion.map(Persion =><PersionInfo name={Persion.name} username={Persion.username} email={Persion.email} ></PersionInfo>)
+     user.map(user =><PersionInfo name={user.name} username={user.username} email={user.email} ></PersionInfo>)
      }
-     
     </div>
   );
 }
