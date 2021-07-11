a573fe4bf4fb5cabde39c7f2eb2174eb82767a86
raihaninfo <raihanmahmudi35@gmail.com>
2021-07-11 10:18:17 +0600

    text change

--- react/core-concepts/src/App.jsx
+++ react/core-concepts/src/App.jsx
@@ -1,15 +1,11 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
       <h2>Hello React</h2>
     </div>
   );
 }
-
-
 
 export default App;
