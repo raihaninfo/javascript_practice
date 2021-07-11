72ab80895610929eec8c81de7422266a6cb97e5e
raihaninfo <raihanmahmudi35@gmail.com>
2021-07-11 10:10:11 +0600

    change old code

--- react/core-concepts/src/App.jsx
+++ react/core-concepts/src/App.jsx
@@ -3,31 +3,13 @@ import "./App.css";
 
 function App() {
 
-  const [user, setUser] = useState([])
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res=>res.json())
-    .then(data =>setUser(data))
-  },[])
-
   return (
     <div className="App">
-     {
-     user.map(user =><PersionInfo name={user.name} key= {user.id} username={user.username} email={user.email} ></PersionInfo>)
-     }
+      <h2>Hello React</h2>
     </div>
   );
 }
 
-function PersionInfo(props){
-  return(
-    <div style={{border:"1px solid red", margin:'10px'}}>
-      <h2>Name: {props.name} </h2>
-      <h3>User Name : {props.username}</h3>
-      <h3>Email : {props.email} </h3>
-    </div>
-  )
-}
 
 
 export default App;
