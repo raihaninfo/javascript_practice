d856cbe45705d602805b7a91bbfced785e310588
raihaninfo <raihanmahmudi35@gmail.com>
2021-07-12 00:04:10 +0600

    new package install

---
+++ react/react-core-concept/src/Data/DataTwo.js
@@ -0,0 +1,121 @@
+var data = [
+    {
+        "id":11,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    },
+    {
+        "id":12,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    },
+    {
+        "id":13,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    },
+    {
+        "id":14,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    },
+    {
+        "id":15,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    },
+    {
+        "id":16,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    },
+    {
+        "id":17,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    },
+    {
+        "id":18,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    },
+    {
+        "id":19,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    },
+    {
+        "id":20,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    },
+    {
+        "id":21,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    },
+    {
+        "id":22,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    },
+    {
+        "id":23,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    },
+    {
+        "id":24,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    },
+    {
+        "id":25,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    },
+    {
+        "id":16,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    },
+    {
+        "id":27,
+        "question": "",
+        "address" : "",
+        "answer": "",
+        "ref": ""
+    }
+]
