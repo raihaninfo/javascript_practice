76170109c1b4bdf8fca526f758457145b8775be8
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-07-18 16:25:12 +0600

    stat and useState

--- react/react-core-concept/src/App.jsx
+++ react/react-core-concept/src/App.jsx
@@ -1,23 +1,14 @@
-import { useState } from "react";
 import "./App.css";
+import Counter from "./components/Counter";
 import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import fakeData from "./fakeData"
 
 function App() {
-  const first10 = fakeData.slice(0, 10);
-  const [products] = useState(first10);
 
   return (
     <div className="App">
       <header className="App-header">
         <Home></Home>
-        {products.map((products) => (
-          <Navbar
-            products={products}
-            key={products.key}
-          ></Navbar>
-        ))}
+        <Counter></Counter>
       </header>
     </div>
   );
