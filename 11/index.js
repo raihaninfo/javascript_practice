0315e3bcc7a49d97300516ed2adec19174dd7fc6
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-08-20 22:56:00 +0600

    code format

--- 11/index.js
+++ 11/index.js
@@ -12,63 +12,62 @@
 // }
 
 var createRect = function (width, height) {
-    return {
-        width: width,
-        height: height,
+  return {
+    width: width,
+    height: height,
 
-        draw: function () {
-            console.log("i am a rectangle")
-            this.printProperties()
-        },
-        printProperties: function () {
-            console.log("My width is " + this.width + " And Height " + this.height)
-        }
-    }
-}
+    draw: function () {
+      console.log("i am a rectangle");
+      this.printProperties();
+    },
+    printProperties: function () {
+      console.log("My width is " + this.width + " And Height " + this.height);
+    },
+  };
+};
 
-var rect1 = createRect(10, 4)
-rect1.draw()
+var rect1 = createRect(10, 4);
+rect1.draw();
 
-var rect2 = createRect(23, 45)
-rect2.draw()
-
+var rect2 = createRect(23, 45);
+rect2.draw();
 
 var Recttangle = function (width, height) {
-    this.width = width
-    this.height = height
+  this.width = width;
+  this.height = height;
 
-    this.draw = function () {
-        console.log("I am a reactangle")
-        this.printProperties()
-        console.log(this)
-    }
-    this.printProperties = function () {
-        console.log("My width is " + this.width + " And Height " + this.height)
-    }
-}
+  this.draw = function () {
+    console.log("I am a reactangle");
+    this.printProperties();
+    console.log(this);
+  };
+  this.printProperties = function () {
+    console.log("My width is " + this.width + " And Height " + this.height);
+  };
+};
 
-var rect3 = new Recttangle(23, 45)
-rect3.draw()
+var rect3 = new Recttangle(23, 45);
+rect3.draw();
 
-var n = 10
+var n = 10;
 
 function change(n) {
-    n = n + 100
-    console.log(n)
+  n = n + 100;
+  console.log(n);
 }
 
-change(n)
-console.log(n)
+change(n);
+console.log(n);
 
 var obj = {
-    a: 10,
-    b: 20
-}
+  a: 10,
+  b: 20,
+};
 
 function changeMe(obj) {
-    obj.a = obj.a + 100
-    obj.b = obj.b + 100
-    console.log(obj)
+  obj.a = obj.a + 100;
+  obj.b = obj.b + 100;
+  console.log(obj);
 }
 
-changeMe(obj)
+changeMe(obj);
