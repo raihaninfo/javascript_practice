4244de66889d215cda02b60ad14ec50d9a7b6487
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-08-20 20:05:16 +0600

    code format

--- 00_1/date.js
+++ 00_1/date.js
@@ -1,10 +1,10 @@
-var date = new Date()
+var date = new Date();
 
-console.log(date.toString())
-console.log(date.toTimeString())
-console.log(date.toLocaleString())
-console.log(date.getFullYear())
-console.log(date.getMonth())
-console.log(date.getDate())
-console.log(date.getMinutes())
-console.log(date.getSeconds())
+console.log(date.toString());
+console.log(date.toTimeString());
+console.log(date.toLocaleString());
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDate());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
