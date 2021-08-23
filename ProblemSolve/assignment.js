bd1467d7c758f247ed92c0e3838f09ef961444c3
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-08-23 20:59:17 +0600

    format code

--- ProblemSolve/assignment.js
+++ ProblemSolve/assignment.js
@@ -6,31 +6,28 @@
  * 10000/5280
  */
 
-
 function feetToMile(feet) {
-    let feetValue = feet / 5280
-    let feetRound = Math.round(feetValue)
-    return feetRound
+  let feetValue = feet / 5280;
+  let feetRound = Math.round(feetValue);
+  return feetRound;
 }
 
-console.log(feetToMile(58558))
-
+console.log(feetToMile(58558));
 
 // wood calculator
 /**
- * আমরা ধরে নিচ্ছি একটি চেয়ারে ১ ছেপটি কাট লাগে, এবং একটি টেবিলে ২ ছেপটি এবং একটু খাটে ৫ ছেপটি, সেই হিসাবে আমাদের function কাজ করবে। 
+ * আমরা ধরে নিচ্ছি একটি চেয়ারে ১ ছেপটি কাট লাগে, এবং একটি টেবিলে ২ ছেপটি এবং একটু খাটে ৫ ছেপটি, সেই হিসাবে আমাদের function কাজ করবে।
  */
 
 function woodCalculator(chair, table, khat) {
-    let chairCl = chair * 1;
-    let tableCl = table * 2;
-    let khatCl = khat * 5;
-    return chairCl + tableCl + khatCl
+  let chairCl = chair * 1;
+  let tableCl = table * 2;
+  let khatCl = khat * 5;
+  return chairCl + tableCl + khatCl;
 }
 
-console.log(woodCalculator(1, 3, 4))
+console.log(woodCalculator(1, 3, 4));
 
 // Brick calculator
 
-
-// tiny friend 
+// tiny friend
