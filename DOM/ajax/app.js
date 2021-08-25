067a0cb38bc037fa4930a0a4e69458fe4669b558
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-08-25 20:00:26 +0600

    code format

--- DOM/ajax/app.js
+++ DOM/ajax/app.js
@@ -1,42 +1,42 @@
-let postForm = document.getElementById('post-form')
-let post = document.getElementById('posts')
-const URL = 'https://jsonplaceholder.typicode.com/posts'
+let postForm = document.getElementById("post-form");
+let post = document.getElementById("posts");
+const URL = "https://jsonplaceholder.typicode.com/posts";
 
-postForm.addEventListener('submit', function (e) {
-    e.preventDefault()
+postForm.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    let title = this.title.value || undefined
-    let body = this.body.value || undefined
+  let title = this.title.value || undefined;
+  let body = this.body.value || undefined;
 
-    if (title && body) {
-        let postObj = {
-            userId: 100,
-            title,
-            body
-        }
-        fetch(URL, {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'Application/JSON'
-                },
-                body: JSON.stringify(postObj)
-            })
-            .then(response => response.json())
-            .then(post => {
-                let item = listItemGenatetor(post)
-                posts.appendChild(item)
-                this.reset()
-            })
-            .catch(e => console.log(e.message))
-    } else {
-        alert('Please Provide Every Detels')
-    }
-})
+  if (title && body) {
+    let postObj = {
+      userId: 100,
+      title,
+      body,
+    };
+    fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-type": "Application/JSON",
+      },
+      body: JSON.stringify(postObj),
+    })
+      .then((response) => response.json())
+      .then((post) => {
+        let item = listItemGenatetor(post);
+        posts.appendChild(item);
+        this.reset();
+      })
+      .catch((e) => console.log(e.message));
+  } else {
+    alert("Please Provide Every Detels");
+  }
+});
 
 function listItemGenatetor(item) {
-    let li = document.createElement('li')
-    li.className = 'list-group-item'
-    li.innerHTML = `${item.id}. ${item.title} By user id - ${item.userId}`
+  let li = document.createElement("li");
+  li.className = "list-group-item";
+  li.innerHTML = `${item.id}. ${item.title} By user id - ${item.userId}`;
 
-    return li
+  return li;
 }
