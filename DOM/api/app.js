d289d8bc4b277cb91076163abde580435cd3f04b
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-08-25 20:00:42 +0600

    code format

--- DOM/api/app.js
+++ DOM/api/app.js
@@ -1,25 +1,24 @@
-let load = document.getElementById('load')
-let postlist = document.getElementById('post')
+let load = document.getElementById("load");
+let postlist = document.getElementById("post");
 
-const URL = 'https://jsonplaceholder.typicode.com/posts'
+const URL = "https://jsonplaceholder.typicode.com/posts";
 
-load.addEventListener('click', function () {
-    fetch(URL)
-        .then(response => response.json())
-        .then(post => {
-
-            post.forEach(post => {
-                let listItem = listItemGenatetor(post)
-                postlist.appendChild(listItem)
-            })
-        })
-        .catch(e => console.log(e.message))
-})
+load.addEventListener("click", function () {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((post) => {
+      post.forEach((post) => {
+        let listItem = listItemGenatetor(post);
+        postlist.appendChild(listItem);
+      });
+    })
+    .catch((e) => console.log(e.message));
+});
 
 function listItemGenatetor(item) {
-    let li = document.createElement('li')
-    li.className = 'list-group-item'
-    li.innerHTML = item.title
+  let li = document.createElement("li");
+  li.className = "list-group-item";
+  li.innerHTML = item.title;
 
-    return li
+  return li;
 }
--- DOM/domEvent/main.js
+++ DOM/domEvent/main.js
@@ -1,24 +1,22 @@
-let header = document.querySelector('h2');
-let span = document.getElementById('span')
+let header = document.querySelector("h2");
+let span = document.getElementById("span");
 
-header.addEventListener('click', function (e) {
-    console.log('Click hoyse re...')
-    span.innerHTML = 'ey tumi click korle kano ?'
-})
+header.addEventListener("click", function (e) {
+  console.log("Click hoyse re...");
+  span.innerHTML = "ey tumi click korle kano ?";
+});
 
-let body = document.getElementById('body')
+let body = document.getElementById("body");
 
+let btn = document.getElementById("btn");
+let btn2 = document.getElementById("btn2");
 
+btn.addEventListener("click", function () {
+  body.style.backgroundColor = "black";
+  body.style.color = "white";
+});
 
-let btn = document.getElementById('btn')
-let btn2 = document.getElementById('btn2')
-
-btn.addEventListener('click', function () {
-    body.style.backgroundColor = 'black';
-    body.style.color = 'white'
-})
-
-btn2.addEventListener('click', function () {
-    body.style.backgroundColor = 'white';
-    body.style.color = 'black'
-})
+btn2.addEventListener("click", function () {
+  body.style.backgroundColor = "white";
+  body.style.color = "black";
+});
