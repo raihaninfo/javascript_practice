953ba502e0a875977dc17d7523f23e88942e760e
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-08-26 22:02:33 +0600

    arrow function profect

--- ES-6/Es6/arrow.js
+++ ES-6/Es6/arrow.js
@@ -38,3 +38,17 @@ let javascript = {
 };
 
 javascript.printLibraries()
+
+const searchInput = document.querySelector(".search");
+const display = document.querySelector(".result");
+const thanks = document.querySelector(".thanks");
+
+function show(){
+  display.innerHTML = this.value;
+  var self = this;
+  setTimeout(function(){
+    thanks.innerHTML = `You have typed: ${self.value}`;
+  },1000)
+}
+
+searchInput.addEventListener("keyup", show)
