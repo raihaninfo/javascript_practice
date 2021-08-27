0d17d9f7cf83271e7380aa2fa687879f7b547be4
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-08-27 16:01:51 +0600

    fizzBuzz problem solve

---
+++ ProblemSolve/FizzBuzz.js
@@ -0,0 +1,19 @@
+// Print integers 1 to N, but print “Fizz” if an integer is divisible by 3,
+//“Buzz” if an integer is divisible by 5,
+//and “FizzBuzz” if an integer is divisible by both 3 and 5.
+
+function FizzBuzz(input) {
+  if (typeof input !== "number") {
+    console.log("Not A Number");
+  } else if (input % 3 === 0 && input % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (input % 3 === 0) {
+    console.log("Fizz");
+  } else if (input % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(input);
+  }
+}
+
+FizzBuzz(2);
