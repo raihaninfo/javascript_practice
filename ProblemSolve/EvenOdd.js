112c3a5b2299c00aa98029723bd2316fd16a661d
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-08-27 19:45:49 +0600

    EVEN and ODD number problem solve

---
+++ ProblemSolve/EvenOdd.js
@@ -0,0 +1,13 @@
+// even and odd number
+
+function showNumber(limit) {
+  for (let i = 0; i <= limit; i++) {
+    if (i % 2 === 0) {
+      console.log(i + " " + "EVEN");
+    } else {
+      console.log(i + " " + "ODD");
+    }
+  }
+}
+
+showNumber(10);
