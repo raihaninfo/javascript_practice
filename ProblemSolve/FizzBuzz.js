6231a56ff8de4e8398ac5469fe92770ed3c4d350
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-08-27 16:03:24 +0600

    fizzBuzz problem solve

--- ProblemSolve/FizzBuzz.js
+++ ProblemSolve/FizzBuzz.js
@@ -1,3 +1,5 @@
+// FizzBuzz Algorithm
+
 // Print integers 1 to N, but print “Fizz” if an integer is divisible by 3,
 //“Buzz” if an integer is divisible by 5,
 //and “FizzBuzz” if an integer is divisible by both 3 and 5.
