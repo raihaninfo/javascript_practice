7d4fa0241a14ffca5f5e78dba0d336821a89bc61
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-09-06 20:13:33 +0600

    format code

--- DOM/app.js
+++ DOM/app.js
@@ -1,30 +1,30 @@
-let form = document.getElementById('form')
+let form = document.getElementById("form");
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault()
-    let isValid = false
-    let formData = {};
-    [...this.elements].forEach(el => {
-        if (el.type !== 'submit') {
-            isValid = validetor(el)
-            if (isValid) {
-                formData[el.name] = el.value
-            } else {
-                // alert(`${el.name} Is Required`)
-                el.classList.add('is-invalid')
-            }
-        }
-    })
-    if (isValid) {
-        console.log(formData)
-        this.reset()
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let isValid = false;
+  let formData = {};
+  [...this.elements].forEach((el) => {
+    if (el.type !== "submit") {
+      isValid = validetor(el);
+      if (isValid) {
+        formData[el.name] = el.value;
+      } else {
+        // alert(`${el.name} Is Required`)
+        el.classList.add("is-invalid");
+      }
     }
-})
+  });
+  if (isValid) {
+    console.log(formData);
+    this.reset();
+  }
+});
 
 function validetor(ele) {
-    if (!ele.value) {
-        return false
-    }
+  if (!ele.value) {
+    return false;
+  }
 
-    return true
+  return true;
 }
