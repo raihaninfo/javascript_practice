0f12e3cd56b50b4c160b03e478b1efd31ba44ffa
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-09-07 20:57:12 +0600

    code format

--- Project/weather-app/index.js
+++ Project/weather-app/index.js
@@ -1,30 +1,32 @@
-const express = require('express')
-const cors = require('cors')
-const BodyParser = require('body-parser')
-const mongoose = require('mongoose')
+const express = require("express");
+const cors = require("cors");
+const BodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.use(BodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+  BodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(BodyParser.json());
 
-app.use('/api/history', require('./api/route'));
+app.use("/api/history", require("./api/route"));
 
-
-const PORT = process.env.PORT || 4444
+const PORT = process.env.PORT || 4444;
 
 app.listen(PORT, () => {
-    console.log('App is Running on PORT' + ' ' + PORT)
-    mongoose.connect('mongodb+srv://raihan:raihan@cluster0.uknw9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-
-
-})
+  console.log("App is Running on PORT" + " " + PORT);
+  mongoose.connect(
+    "mongodb+srv://raihan:raihan@cluster0.uknw9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+});
